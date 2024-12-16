3dc1a18734544bb257e846ce6eacc1c55331516a
mariaermolova <47752002+mariaermolova@users.noreply.github.com>
2024-12-16 13:54:59 +0100

    added documentation

--- MEPtoList.m
+++ MEPtoList.m
@@ -1,8 +1,11 @@
-%% Reorder the MEP from a cell array into a list (i.e. table)
-load(fullfile('W:\Experimental Data\2019-04 M1M1PAS (processed)\Summary files',...
-    'SIHI_data_16subj_with_Rearranged.mat'),'MEP','INT')
-% "MEP" is (nSubjects, nConditions, nTimes, nChannels)
+%% Reorder the MEP/SIHI data from a cell array into a list (i.e. table)
+% "MEP" cell array is (nSubjects, nConditions, nTimes, nChannels)
+% Transform it to a list to be used in R
 
+load(fullfile('W:\Projects\2019-04 M1M1PAS Project\analysis',...
+    'SIHI_data_16subj_with_Rearranged.mat'),'MEP','INT')
+
+%Initiate variables
 Subject = [];
 Intervention = [];
 Time = [];
@@ -10,25 +13,38 @@ Channel = [];
 Intensity = [];
 Response = [];
 
+%Set levels of variables
 Times = ["Pre","0","30","60"];
 Interventions = ["negneg","posneg","negpos","random"];
 Channels = ["APBr","FDIr","APBl","FDIl"]; %check it
 
+%loop through subjects
 for iSubject = 1:size(MEP, 1)
+    %loop through interventions
     for iIntervention = 1:size(MEP,2)
+        %loop through runs
         for iTime = 1:size(MEP, 3)
+            %loop through channels
             for iChannel = 1:size(MEP,4)
+                %get the number of subselected data samples
                 nSamples = length(MEP{iSubject, iIntervention, iTime, iChannel});
+                %SIHI values
                 Response = [Response; MEP{iSubject, iIntervention, iTime, iChannel}];
+                %stimulation intensity values
                 Intensity = [Intensity; INT{iSubject, iIntervention, iTime, iChannel}'];
+                %repeat subject id
                 Subject = [Subject; repmat(sprintf("sub-%03.f", iSubject), nSamples, 1)];
+                %repeat run id
                 Time = [Time; repmat(Times(iTime), nSamples, 1)];
+                %repeat intervention id
                 Intervention = [Intervention;repmat(Interventions(iIntervention), nSamples, 1)];
+                %repeat EMG channel id
                 Channel = [Channel;repmat(Channels(iChannel), nSamples, 1)];
             end
         end
     end
 end
 
+%store everything in a table
 MEPdata = table(Subject, Intervention, Time, Channel, Intensity, Response);
 writetable(MEPdata,'MEPdata.csv');
