17f6d3f57d850a42cd34654122e125e24132723b
mariaermolova <47752002+mariaermolova@users.noreply.github.com>
2024-12-16 14:03:26 +0100

    added documantation

--- check_ica_comps.m
+++ check_ica_comps.m
@@ -1,42 +1,44 @@
+%% Remove artifactial ICA components
+% Look through the ICA components, manually adjust selection for removal,
+% remove them from data
 
-% % Load STUDY and ALLEEG
+% Load STUDY and ALLEEG
 % [STUDY, ALLEEG] = pop_loadstudy('filename', 'study_name.study');
 
-% ALLEEG2 = ALLEEG;
-
-% Loop through each dataset
+%% Loop through each dataset and select components
 for i = 1:length(ALLEEG)
 
     sprintf('Processing dataset %d',i)
 
-EEG = ALLEEG(i);
-
-% Plot ICA components for visual inspection
-pop_selectcomps(EEG, 1:30);  % Displays first 30 components
-
-% Wait for user to make changes and close the GUI
-uiwait(gcf);  % This will pause the execution until the GUI is closed
-
-% Store the updated EEG dataset back into ALLEEG
-[ALLEEG, EEG, CURRENTSET] = eeg_store(ALLEEG, EEG, i);
-end
-%%
-% Loop through each dataset and remove bad components
-for i = 1:length(ALLEEG)
+    % Select one dataset
     EEG = ALLEEG(i);
-    
+
+    % Plot ICA components for visual inspection
+    pop_selectcomps(EEG, 1:30);  % Displays first 30 components
+    uiwait(gcf); 
+
+    % Store the updated EEG dataset back into ALLEEG
+    [ALLEEG, EEG, CURRENTSET] = eeg_store(ALLEEG, EEG, i);
+end
+%% Loop through each dataset and remove bad components
+
+for i = 1:length(ALLEEG)
+
+    EEG = ALLEEG(i);
+
     % Remove bad components based on flags
-    EEG = pop_subcomp(EEG, find(EEG.reject.gcompreject), 0);  
-    
+    EEG = pop_subcomp(EEG, find(EEG.reject.gcompreject), 0);
+
     % Save the modified dataset back to ALLEEG
     ALLEEG(i) = EEG;
 end
 
 % Update the STUDY structure
 STUDY = std_checkset(STUDY, ALLEEG);
-%%
-% Save the modified datasets
+%% Save the modified datasets
+
 for i = 1:length(ALLEEG)
+
     pop_saveset(ALLEEG(i), 'filename', ALLEEG(i).filename);
 end
 
