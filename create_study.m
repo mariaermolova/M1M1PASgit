da14c078693642d064ea97fc076e27ed389e26ee
mariaermolova <47752002+mariaermolova@users.noreply.github.com>
2024-12-16 16:50:05 +0100

    added documentation

--- create_study.m
+++ create_study.m
@@ -1,4 +1,5 @@
-%% Create an EEGLAB Study structure (to use automatic batch processing)
+%% Create an EEGLAB STUDY structure (to use automatic batch processing)
+%existing STUDY structure was created with create_study.m
 
 % % To do this manually
 %[ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
@@ -8,23 +9,26 @@
 %     {'index',2,'load','W:\\Experimental Data\\2019-04 M1M1PAS (processed)\\EEG_RS\\M1M1PAS_001-negneg-eeg_eo-2.set',...
 %     'subject','sub-001','session',1,'run',2,'condition','negneg','task','negneg'}});
 
+% one dataset loaded at a time
 pop_editoptions( 'option_storedisk', 1);
 
 commands = {};
 
-% Loop through all of the subjects in the study to create the dataset
-for loopnum = 1:length(STUDY.datasetinfo) %for each subject
+% Loop through all of the subjects in the study
+for loopnum = 1:length(STUDY.datasetinfo) 
 
+    %get info from the existing STUDY structure
     dataFile = STUDY.datasetinfo(loopnum);
 
+    %store info in the commands parameter
     commands = {commands{:} ...
     {'index' dataFile.index 'load' dataFile.filepath 'subject' dataFile.subject 'condition' dataFile.condition...
     'session' dataFile.session 'run' dataFile.run 'task' dataFile.task}};
 end
 
+%create new STUDY structure with the commands parameters
 [STUDY, ALLEEG] = std_editset(STUDY, ALLEEG, 'name','M1M1PAS','commands',commands,'updatedat','on');
 
-% Update workspace variables and redraw EEGLAB
 CURRENTSTUDY = 1; EEG = ALLEEG; CURRENTSET = [1:length(EEG)];
 [STUDY, ALLEEG] = std_checkset(STUDY, ALLEEG);
 eeglab redraw
